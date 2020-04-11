12520e3e11f764793e47f619c70c0e9b4186daf1
Chua Qi Yang <qymehmeh@gmail.com>
2020-04-11 13:59:56 +0800

    Change setup using EJS files to cater for render

--- server.js
+++ server.js
@@ -1,36 +1,29 @@
-// Copyright 2017 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 'use strict';
 
-// [START gae_node_request_example]
+// requires
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require(`path`);
 const util = require(`util`);
 
+//setup ExpressJS
+//body parser to parse JSON
+//serve static files
+//set View Engine to EJS files
+//set views filepath
 const app = express();
-// app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.set("view engine", "ejs"); 
+app.set("views", __dirname + "/views");
+
 var counter = 0;
 var roomlist = [];
 
 var securityNonce = [];
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/mainmenu.html'));
+  res.render('mainmenu');
 });
 
 app.get('/getChatLog', (req, res) => {
@@ -38,10 +31,10 @@ app.get('/getChatLog', (req, res) => {
 });
 
 app.post('/room', (req, res) => {
-  let index = securityNonce.indexOf(req.nonce);
+  let index = securityNonce.indexOf(req.body.nonce);
   if (index >= 0) {
     securityNonce.splice(index, 1);
-    res.send(`In Room ID: ${req.roomID}`);
+    res.render("gamescreen", {roomID: req.body.roomID});
   } else {
     res.send("Bad activity detected");
   }
